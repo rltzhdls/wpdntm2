a4dc6f7aea1732f7ce878ed6111168d1ae9081ac
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-28 15:21:09 +0900

    Add files via upload

---
+++ 75a3cd.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/wpdntm1@main/zeus.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
